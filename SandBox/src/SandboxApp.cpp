fd15296f60a5541b973e18fe7567307d0deb4789
Lazarus-glhf <redrumlivedog@outlook.com>
2023-05-12 22:06:00 +0800

    Timesteps & transform (add missing sandbox.cpp

--- SandBox/src/SandboxApp.cpp
+++ SandBox/src/SandboxApp.cpp
@@ -2,41 +2,191 @@
 
  #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Poto::Layer
 {
 public:
 	ExampleLayer()
-		: Layer("Example")
+		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f)
 	{
+		m_VertexArray.reset(Poto::VertexArray::Create());
+
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
+		};
+
+		std::shared_ptr<Poto::VertexBuffer> vertexBuffer;
+		vertexBuffer.reset(Poto::VertexBuffer::Create(vertices, sizeof(vertices)));
+		Poto::BufferLayout layout = {
+			{ Poto::ShaderDataType::Float3, "a_Position" },
+			{ Poto::ShaderDataType::Float4, "a_Color" }
+		};
+		vertexBuffer->SetLayout(layout);
+		m_VertexArray->AddVertexBuffer(vertexBuffer);
+
+		uint32_t indices[3] = { 0, 1, 2 };
+		std::shared_ptr<Poto::IndexBuffer> indexBuffer;
+		indexBuffer.reset(Poto::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
+		m_VertexArray->SetIndexBuffer(indexBuffer);
+
+		m_SquareVA.reset(Poto::VertexArray::Create());
+
+		float squareVertices[3 * 4] = {
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
+		};
+
+		std::shared_ptr<Poto::VertexBuffer> squareVB;
+		squareVB.reset(Poto::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
+		squareVB->SetLayout({
+			{ Poto::ShaderDataType::Float3, "a_Position" }
+			});
+		m_SquareVA->AddVertexBuffer(squareVB);
+
+		uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
+		std::shared_ptr<Poto::IndexBuffer> squareIB;
+		squareIB.reset(Poto::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
+		m_SquareVA->SetIndexBuffer(squareIB);
+
+		std::string vertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
+			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
+
+			out vec3 v_Position;
+			out vec4 v_Color;
+
+			void main()
+			{
+				v_Position = a_Position;
+				v_Color = a_Color;
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
+			}
+		)";
+
+		std::string fragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+			in vec4 v_Color;
+
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
+			}
+		)";
+
+		m_Shader.reset(new Poto::Shader(vertexSrc, fragmentSrc));
+
+		std::string blueShaderVertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+
+			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
+
+			out vec3 v_Position;
+
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
+			}
+		)";
+
+		std::string blueShaderFragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+			in vec3 v_Position;
+			void main()
+			{
+				color = vec4(0.2, 0.3, 0.8, 1.0);
+			}
+		)";
+
+		m_BlueShader.reset(new Poto::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
 	}
 
-	void OnUpdate() override
+	void OnUpdate(Poto::Timestep ts) override
 	{
-		if (Poto::Input::IsKeyPressed(PT_KEY_TAB))
+		if (Poto::Input::IsKeyPressed(PT_KEY_LEFT))
+			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
+		else if (Poto::Input::IsKeyPressed(PT_KEY_RIGHT))
+			m_CameraPosition.x += m_CameraMoveSpeed * ts;
+
+		if (Poto::Input::IsKeyPressed(PT_KEY_UP))
+			m_CameraPosition.y += m_CameraMoveSpeed * ts;
+		else if (Poto::Input::IsKeyPressed(PT_KEY_DOWN))
+			m_CameraPosition.y -= m_CameraMoveSpeed * ts;
+
+		if (Poto::Input::IsKeyPressed(PT_KEY_A))
+			m_CameraRotation += m_CameraRotationSpeed * ts;
+		if (Poto::Input::IsKeyPressed(PT_KEY_D))
+			m_CameraRotation -= m_CameraRotationSpeed * ts;
+
+		Poto::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
+		Poto::RenderCommand::Clear();
+
+		m_Camera.SetPosition(m_CameraPosition);
+		m_Camera.SetRotation(m_CameraRotation);
+
+		Poto::Renderer::BeginScene(m_Camera);
+
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++)
 		{
-			PT_TRACE("Tab key is pressed (poll)!");
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Poto::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
 		}
+
+		Poto::Renderer::Submit(m_Shader, m_VertexArray);
+
+		Poto::Renderer::EndScene();
 	}
 
 	virtual void OnImGuiRender() override
 	{
-		ImGui::Begin("Test");
-		ImGui::Text("Hello World");
-		ImGui::End();
+	
 	}
 
 	void OnEnvent(Poto::Event& event) override
 	{
-		if (event.GetEventType() == Poto::EventType::KeyPressed)
-		{
-			Poto::KeyPressedEvent& e = (Poto::KeyPressedEvent&)event;
-			if (e.GetKeyCode() == PT_KEY_TAB)
-			{
-				PT_TRACE("Tab key is pressed (event)!");
-			}
-			PT_TRACE("{0}", (char)e.GetKeyCode());
-		}
+	
 	}
+
+private:
+	std::shared_ptr<Poto::Shader> m_Shader;
+	std::shared_ptr<Poto::VertexArray> m_VertexArray;
+
+	std::shared_ptr<Poto::Shader> m_BlueShader;
+	std::shared_ptr<Poto::VertexArray> m_SquareVA;
+
+	Poto::OrthographicCamera m_Camera;
+	glm::vec3 m_CameraPosition;
+	float m_CameraMoveSpeed = 5.0f;
+
+	float m_CameraRotation = 0.0f;
+	float m_CameraRotationSpeed = 180.0f;
 };
 
 class Sandbox : public Poto::Application
